32ba47c3d01f1ce1e29d297591165e9a62bce910
ZhBexultan <zh.beksultan1412@mail.ru>
2020-06-13 15:56:37 +0600

    done preproject 2.1.3

---
+++ 1/2.1.3. spring-beans/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import app.model.Animal;
+import app.model.Timer;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    @Autowired
+    private MyTimer myTimer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.print("At: ");
+        System.out.println(myTimer.getTime());
+        System.out.println("________________________");
+    }
+}
---
+++ 1/2.1.3. spring-beans/src/main/java/app/model/MyTimer.java
@@ -0,0 +1,8 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTimer extends Timer {
+
+}
