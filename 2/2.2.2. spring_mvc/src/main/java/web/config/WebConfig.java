d3412ec6bcf7e7066ea2f953b06c83e52272e4ab
ZhBexultan <zh.beksultan1412@mail.ru>
2020-06-21 23:28:03 +0600

    done preproject-2.2.2(100%)

--- 2/2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2/2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -1,12 +1,18 @@
 package web.config;
 
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -30,4 +36,23 @@ public class WebConfig implements WebMvcConfigurer {
         return resolver;
     }
 
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("languages/locale");
+        messageSource.setDefaultEncoding("CP1251");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
 }
--- 2/2.2.2. spring_mvc/src/webapp/WEB-INF/pages/cars.jsp
+++ 2/2.2.2. spring_mvc/src/webapp/WEB-INF/pages/cars.jsp
@@ -1,18 +1,23 @@
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Cars</title>
+    <title><fmt:message key="locale.title"/></title>
 </head>
 <body>
-    <h2>Cars</h2>
+    <ul>
+        <li><a href="?lang=en"><fmt:message key="locale.lang.en"/></a></li>
+        <li><a href="?lang=ru"><fmt:message key="locale.lang.ru"/></a></li>
+    </ul>
+    <h2><fmt:message key="locale.title"/></h2>
     <table>
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Series</th>
-                <th>Year</th>
+                <th><fmt:message key="locale.table.name"/></th>
+                <th><fmt:message key="locale.table.series"/></th>
+                <th><fmt:message key="locale.table.year"/></th>
             </tr>
         </thead>
         <c:if test="${cars != null}">
@@ -29,6 +34,6 @@
         </c:if>
     </table>
     <br>
-    <a href="/hello">Back</a>
+    <a href="/hello"><fmt:message key="locale.back"/></a>
 </body>
 </html>
